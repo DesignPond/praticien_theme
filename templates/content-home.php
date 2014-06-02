4f00ae61fd8aac133616d5516fe845b90ba6132f
Cindy Leschaud <cindy.leschaud@gmail.com>
2014-06-02 11:14:45 +0200

    change of header and link bloc

--- templates/content-home.php
+++ templates/content-home.php
@@ -1,11 +1,19 @@
-<div class="row">
-  <div class="col-md-4">
-	  <div class="bloc"></div>
-  </div>
-  <div class="col-md-4">
-	  <div class="bloc"></div>
-  </div>
-  <div class="col-md-4">
-	  <div class="bloc"></div>
-  </div>
+<div id="homeContent">
+	<div class="row">
+	  <div class="col-md-4">
+		  <div class="bloc">
+			  <h3>Derniers arrêts rendus</h3>
+		  </div>
+	  </div>
+	  <div class="col-md-4">
+		  <div class="bloc">
+			  <h3>Arrêts destinés à la publication</h3>
+		  </div>
+	  </div>
+	  <div class="col-md-4">
+		  <div class="bloc">
+			  <h3>Newsletter</h3>
+		  </div>
+	  </div>
+	</div>
 </div>
--- templates/home-header.php
+++ templates/home-header.php
@@ -1,10 +1,27 @@
 <div id="home-header">
 	<div class="container">
 		<div class="row">
+		
+			<div class="col-sm-6">
+			
+				<div class="row">
+					<div class="col-sm-5">
+						<a href="http://www.publications-droit.ch/#/cat/publications/item/229" target="_blank">
+							<img src="<?php echo get_bloginfo('template_directory');?>/assets/img/book.png" alt="droit pour le praticien" />
+						</a>
+					</div>
+					<div class="col-sm-7 buythebook">
+						<p>Obtenez l’accès au site pour l’année en cours grâce au code inscrit sur la dernière édition du livre.</p>
+						<a href="http://www.publications-droit.ch/#/cat/publications/item/229" target="_blank" class="btn btn-buy">Acheter sur www.publications-droit.ch</a>
+					</div>
+				</div>	
+					
+			</div>
+						
 			<div class="col-sm-6">
 				
-				<div id="login">
-				<h2>S'identifier</h2>
+				<div id="login" class="pull-right">
+				<h2>login</h2>
 				
 				<?php
 				
@@ -30,27 +47,16 @@
 				</div>
 								
 			</div>
-			<div class="col-sm-6">
-			
-				<div class="row">
-					<div class="col-sm-5">
-						<a href="http://www.publications-droit.ch/#/cat/publications/item/229" target="_blank">
-							<img src="<?php echo get_bloginfo('template_directory');?>/assets/img/book.png" alt="droit pour le praticien" />
-						</a>
-					</div>
-					<div class="col-sm-7 buythebook">
-						<p>Obtenez l’accès au site pour l’année en cours grâce au code inscrit sur la dernière édition du livre.</p>
-						<a href="http://www.publications-droit.ch/#/cat/publications/item/229" target="_blank" class="btn btn-buy">Acheter sur www.publications-droit.ch</a>
-					</div>
-				</div>	
-				
-			</div>
 		</div>
+				
+	</div>	
+</div>
 
+<div id="home-arrets-links">
+	<div class="container">
 		<div class="row">
 		   <div class="col-sm-6 text-center"><a href="" class="arrets-links-bloc"><span>Consulter</span><br/>Arrêts principaux résumés</a></div>
 		   <div class="col-sm-6 text-center"><a href="" class="arrets-links-bloc"><span>Consulter</span><br/>Autres arrêts</a></div>
 		</div>
-
 	</div>	
 </div>
